4d8d19d28edd3c5cd0499be9c8c719cb7c0b323e
TA aMaker <thierry@accart.name>
2025-05-31 19:38:43 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+UnleashTheBricks.onMessageStopReceived(function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        # # . # #
+        # # . # #
+        # # . # #
+        `)
+})
+UnleashTheBricks.onMessageDangerReceived(function () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    UnleashTheBricks.incrementRadioGroup()
+})
+UnleashTheBricks.onMessageStartReceived(function () {
+    basic.showLeds(`
+        . # . . .
+        . # # . .
+        . # # # .
+        . # # . .
+        . # . . .
+        `)
+})
+UnleashTheBricks.setEchoToConsole(true)
+UnleashTheBricks.initCommunicationChannel(CommunicationChannel.Radio)
+UnleashTheBricks.emitStatus()
+UnleashTheBricks.setBotStatus(BotStatus.Searching)
+UnleashTheBricks.emitStatus()
+basic.forever(function () {
+    UnleashTheBricks.emitHeartBeat()
+    control.waitMicros(10000000)
+})
